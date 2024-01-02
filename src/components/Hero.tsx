651491d3f1ed9e9c43aa88867f9b030d631aea9c
Abeera Tahir <abeeratahir2018@gmail.com>
2024-01-02 19:05:19 +0500

    Fix responsiveness of the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
   }, []);
   return (
     <>
-      <section className="mt-[2rem] h-screen pl-8 md:pl-20 pr-9 w-full bg-[#041125] text-white">
+      <section className="mt-[2rem] h-screen pl-8 md:pl-16 lg:pl-20 pr-9 w-full bg-[#041125] text-white">
         <div className="h-full flex flex-col md:flex-row gap-10 md:gap-4 justify-center items-center">
           <div className="w-full flex flex-col gap-8">
             <motion.h1
@@ -29,7 +29,7 @@ const Hero = () => {
               initial="hidden"
               animate="visible"
               transition={{ duration: 1.5, delay: 0, ease: "easeInOut" }}
-              className="text-[1.875rem] md:text-[3.125rem] font-[600] md:leading-[4.1rem]">
+              className="text-[1.875rem] md:text-[2.5rem] lg:text-[3.125rem] font-[600] md:leading-[4.1rem]">
               Discover the World of Cryptocurrency
             </motion.h1>
             <motion.h2
@@ -40,7 +40,7 @@ const Hero = () => {
               initial="hidden"
               animate="visible"
               transition={{ duration: 1.5, delay: 0.75, ease: "easeInOut" }}
-              className="text-[0.975rem] md:text-[1.5rem] font-[400] leading-7 md:leading-[2.5rem]">
+              className="text-[0.975rem] md:text-[1.25rem] lg:text-[1.5rem] font-[400] leading-7 md:leading-[2.5rem]">
               Bringing You Comprehensive Crypto Information, Market Trends, and
               Pricing History.
             </motion.h2>
@@ -68,7 +68,7 @@ const Hero = () => {
             initial="hidden"
             animate="visible"
             transition={{ duration: 1.5, delay: 1.5, ease: "easeInOut" }}
-            className="md:min-w-[550px] min-w-[270px] pl-3 md:pl-0">
+            className="lg:min-w-[550px] md:min-w-[400px] min-w-[270px] max-w-[450px] pl-3 md:pl-0">
             <img src={bg} alt="crypto image" />
           </motion.div>
         </div>
